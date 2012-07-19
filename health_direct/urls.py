07fddc77e9b330b3e2667313f22d6ca74c4641a7
NickStephens <nick.stephens@server>
2012-07-19 16:05:41 -0700

    I hope this will result in the moving and removing of files, but it may make github a whole lot messier.

--- health_direct/urls.py
+++ health_direct/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from health_direct.health_direct.views import home, inputsearch, questionbuilder, questionbuilt
+from health_direct.views import home, inputsearch, questionbuilder, questionbuilt
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
