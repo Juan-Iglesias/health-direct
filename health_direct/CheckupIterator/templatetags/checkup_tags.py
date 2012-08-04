40a7f2dca62ba121298d46f7d83817e23f4f867d
NickStephens <nick.stephens@server>
2012-08-04 14:57:24 -0700

    Checkup_tags commit.

---
+++ health_direct/CheckupIterator/templatetags/checkup_tags.py
@@ -0,0 +1,42 @@
+'''
+Created on Aug 3, 2012
+
+@author: juan
+'''
+from django.template.loader import get_template
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.tag(name="CheckupDisplay")
+def do_CheckupDisplay(parser, token):
+    try:
+        tag_name, appName, checkupId = token.split_contents()
+    except ValueError:
+        msg = '%r tag requires two arguments' % token.split_contents()[0]
+        raise template.TemplateSyntaxError(msg)
+    return CheckupDisplayNode(appName, checkupId)
+         
+
+class CheckupDisplayNode(template.Node):
+    def __init__(self,appName,checkupId):
+        self.appName = appName
+        self.checkupId = checkupId
+    def render(self,context): #Determine how to correctly use context parameter
+        #if self.app not in settings.INSTALLED_APPS:
+        #    return '' # @todo: raise an error here 
+        
+        # From djangobook:
+        # Like template filters, these rendering functions should fail
+        # silently instead of raising errors. The only time that template
+        # tags are allowed to raise errors is at compilation time.
+        
+        # find self.app (maybe it will be a path)
+        # find template folder for app.path
+        template = str(self.appName) + '.html'
+        t = get_template(str(self.appName) + '/templates/' + template)
+        
+        #Now we're tasked with filling this context with the data we'll receive from the id
+        
+        return t.render(context)
