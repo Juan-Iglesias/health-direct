8f795bac00c1ebefe41844ac4746b86277114a94
Juan Iglesias <juan@Juans-Mac-mini.local>
2012-08-12 21:26:12 -0700

    Checkup iterator pulls from checkup database and calls the corresponding app

--- health_direct/CheckupIterator/views.py
+++ health_direct/CheckupIterator/views.py
@@ -1,16 +1,31 @@
-from health_direct.InputSubmitBackend.models import Input, Apps
+from health_direct.InputSubmitBackend.models import Input
+from health_direct.UserHandler.models import User_Input
 import os
+from django.shortcuts import render_to_response
+from django.utils import importlib
 
-def get_checkup(record):
-    # Looks up app_foreignkey in the record
-    # Get appcode and app out of record
-    a1 = Apps.objects.get(name=record.app)
+def get_checkup(httprequest):
+    
+    nextInput = queuer(httprequest)
+    render_to_response('tagtester.html',{'appName': nextInput['appName']})
+    try:
+        appModule = importlib.import_module('health_direct.'+ nextInput['appName'] + '.views')
+    except ImportError, e:
+        raise e
+    appModule.display(nextInput['checkupId'])
+    
+    
     # calls program at location with the id of the particular checkup as the argument
     # Runs app within checkupiterator window.
-    os.system(str(a1.location) + ' ' + str(record.app_code))
+    
     # When the app completes running it sends the result to User_Entries table
     
-def queuer(request):
+def queuer(httprequest, checkupSet=None):
+    if checkupSet is None:
+        #@todo: Replace this with a checkup populating function
+        checkupSet = User_Input.objects.filter(user = httprequest.user, isCheckup = True).iterator()
+    return checkupSet.next().value('appName','inputId')
+    
     # if (request.session['member_id']) if it exists
     #     User_Input.objects.get(user=request.session['member_id'])
     #     select the most appropriate input (this will return an input primary key)
